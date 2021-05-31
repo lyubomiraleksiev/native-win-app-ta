9c8114421fa574f20d516a5a09cdc36a7f9bc3aa
lyubomiraleksiev-abertay <60470633+lyubomiraleksiev-abertay@users.noreply.github.com>
2021-05-31 19:26:54 +0300

    Fixed line spacing in config file

--- appium.config.ts
+++ appium.config.ts
@@ -4,15 +4,15 @@ import { driver, windowsAppDriverCapabilities } from 'selenium-appium'
 const appId = '"C:/Users/Auxcode/AppData/Local/Programs/Fiddler Everywhere/Fiddler Everywhere.exe"'
 
 class TestEnvironment {
-  setup() {
-    jest.setTimeout(60000);
-    const capabilities = windowsAppDriverCapabilities(appId);
-    return driver.startWithCapabilities(capabilities);
-  }
+    setup() {
+        jest.setTimeout(60000);
+        const capabilities = windowsAppDriverCapabilities(appId);
+        return driver.startWithCapabilities(capabilities);
+    }
 
-  teardown() {
-    return driver.quit();
-  }
+    teardown() {
+        return driver.quit();
+    }
 }
 
 export default new TestEnvironment();
